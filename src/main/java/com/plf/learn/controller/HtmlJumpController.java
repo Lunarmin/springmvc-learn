142de8a6bcf82adcce7551a65e35d82eea735566
Panlf <liangfeng_pan@163.com>
2019-10-14 14:08:59 +0800

    add image

---
+++ src/main/java/com/plf/learn/controller/HtmlJumpController.java
@@ -0,0 +1,14 @@
+package com.plf.learn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/jump")
+public class HtmlJumpController {
+
+	@RequestMapping("/image")
+	public String toImageHtml(){
+		return "image";
+	}
+}
---
+++ src/main/java/com/plf/learn/controller/ImageCodeController.java
@@ -0,0 +1,33 @@
+package com.plf.learn.controller;
+
+import java.awt.image.RenderedImage;
+import java.io.IOException;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.plf.learn.utils.CodeImage;
+
+
+@Controller
+@RequestMapping("/vertify")
+public class ImageCodeController {
+	
+	@RequestMapping("getCode")
+	public void  getCodeImage(HttpServletRequest request, HttpServletResponse response,HttpSession session){
+		Map<String,Object> map = CodeImage.getCodeImage();
+		session.setAttribute("vertifycode", map.get("code"));
+		try {
+			ImageIO.write((RenderedImage) map.get("image"), "JPEG", response.getOutputStream());
+	        response.getOutputStream().flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
