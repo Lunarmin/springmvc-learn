b7444677e9be4f6cc1bafac4184e484bf14c3e1f
Panlf <liangfeng_pan@163.com>
2020-02-24 08:40:04 +0800

    edit log4j 1 to 2

--- src/main/java/com/plf/learn/controller/ImageCodeController.java
+++ src/main/java/com/plf/learn/controller/ImageCodeController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,10 +20,12 @@ import com.plf.learn.utils.CodeImage;
 @Controller
 @RequestMapping("/vertify")
 public class ImageCodeController {
+	private final static Logger log = LoggerFactory.getLogger(ZtreeController.class);
 	
 	@RequestMapping("getCode")
 	public void  getCodeImage(HttpServletRequest request, HttpServletResponse response,HttpSession session){
 		Map<String,Object> map = CodeImage.getCodeImage();
+		log.info("获取到Code为{}",map.get("code"));
 		session.setAttribute("vertifycode", map.get("code"));
 		try {
 			ImageIO.write((RenderedImage) map.get("image"), "JPEG", response.getOutputStream());
--- src/main/java/com/plf/learn/controller/ZtreeController.java
+++ src/main/java/com/plf/learn/controller/ZtreeController.java
@@ -1,5 +1,7 @@
 package com.plf.learn.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("ztree")
 public class ZtreeController {
 
+	private final static Logger log = LoggerFactory.getLogger(ZtreeController.class);
+	
 	@RequestMapping("toztree")
 	public String toZtree(){
+		log.info("进入Ztree测试页面 ");
 		return "ztree";
 	}
 }
